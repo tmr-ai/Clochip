b8a3c01f4fc6d7820783710a97d50513f3790df0
tmr-ai <tim.rahlmeier@gmail.com>
2022-06-19 10:44:09 +0200

    new_item use case and nfc write update
    First steps of the new_item use case are realized. Also NFC write and read should work better now.

---
+++ client/clochip/README.md
@@ -0,0 +1,13 @@
+## Run App on your device:
+
+1. Run a single to install the App:
+
+    `npm install`
+
+2. Run a single command to build for android:
+
+    `npm run android`
+
+
+
+Note: You need Android Studio for it to work. After Android Studio was opened, the gradle should build automatically. Then you need to select your target device and run the App on it. 
--- client/clochip/src/app/tabs/tabs.page.ts
+++ client/clochip/src/app/tabs/tabs.page.ts
@@ -13,6 +13,7 @@ export class TabsPage {
   tag = null
   loader = null
   openCreateModal = false
+  detected = false
 
   constructor(
     private nfc: NFC,
@@ -32,15 +33,17 @@ export class TabsPage {
   async performOperation() {
     // check if payload exist with nDefMessage
     if (
-      this.tag.nDefMessage &&
-      this.tag.nDefMessage.length &&
-      this.tag.nDefMessage.length > 0 &&
-      this.tag.nDefMessage[0].payload
+      this.tag.ndefMessage &&
+      this.tag.ndefMessage.length &&
+      this.tag.ndefMessage.length > 0 &&
+      this.tag.ndefMessage[0].payload
     ) {
-      // get if item is dirty or not
+      alert('Payload detected: ' + JSON.stringify(this.tag.ndefMessage[0].payload))
+      // get if its dirty or not
       const dirtyOrNotResult = await this.getDirtyOrNot()
       console.log(dirtyOrNotResult)
       if (dirtyOrNotResult) {
+        alert('Your choice: ' + dirtyOrNotResult)
         // now store in local database for now (later mysql api)
         var tags = []
         try {
@@ -48,6 +51,7 @@ export class TabsPage {
         } catch(e) {
           tags = []
         }
+        alert('Local db count: ' + tags.length.toString())
         var newDetails = {
           id: this.tag.id,
           status: dirtyOrNotResult,
@@ -55,21 +59,37 @@ export class TabsPage {
         }
         tags.push(newDetails)
         localStorage.setItem('clothes', JSON.stringify(tags))
+        alert('Local db updated with new entry: ' + JSON.stringify(newDetails))
       }
     } else {
-      // no payload ask server for new id
+      alert('Tag is empty!')
+      // no payload ask server for new
       const payload = new Date().getTime()
-      console.log(payload)
-      // now write this id on the tag
-      var message = [
-        this.ndef.textRecord(payload.toString()),
-      ];
-      this.nfc.write(message).then((res) => {
-        // create the form for input (new modal)
-        this.openCreateModal = true
-      }).catch((err) => {
-        alert('Issue in writing to NFC')
-      })
+      alert('New payload generated: ' + payload.toString())
+
+      if (this.tag.isWritable) {
+        // now write this info on the tag
+        try {
+          var message = [
+            this.ndef.mimeMediaRecord('text/plain', payload.toString()),
+          ];
+        } catch(e) {
+          alert(JSON.stringify(e))
+        }
+
+        alert('Message created!')
+
+        this.nfc.write(message).then((res) => {
+          // create the form for input (new modal)
+          this.openCreateModal = true
+          alert('New data is written to tag')
+        }).catch((err) => {
+          alert('Issue in writing to NFC')
+        })
+      } else {
+        alert('NFC tag is not writable!')
+      }
+
     }
 
   }
@@ -103,27 +123,46 @@ export class TabsPage {
   async readNfc() {
     const info = await Device.getInfo();
 
+    this.detected = false
+
     await this.presentLoading('Scanning NFC...')
 
     if (info.platform === 'android') {
-      this.flags = this.nfc.FLAG_READER_NFC_A | this.nfc.FLAG_READER_NFC_V;
-      this.nfc.readerMode(this.flags).subscribe(
-        tag => {
-          this.tag = JSON.stringify(tag)
-          this.performOperation()
-          this.loader.dismiss()
-        },
-        err => {
-          alert(JSON.stringify(err))
-          this.loader.dismiss()
-        })
+      // this.flags = this.nfc.FLAG_READER_NFC_A | this.nfc.FLAG_READER_NFC_V;
+      // this.nfc.readerMode(this.flags).subscribe(
+      //   tag => {
+      //     this.tag = tag
+      //     this.loader.dismiss()
+      //   },
+      //   err => {
+      //     alert(JSON.stringify(err))
+      //     this.loader.dismiss()
+      //   })
+      this.nfc.addNdefListener((s) => {
+      }, (err) => {
+        alert('Error in starting NFC reader: ' + JSON.stringify(err))
+      }).subscribe((res) => {
+        if (this.detected) {
+          return
+        }
+        alert(JSON.stringify(res))
+        this.loader.dismiss()
+        this.tag = res.tag
+        this.detected = true
+        this.performOperation()
+      }, (e) => {
+        alert('err from subs' + JSON.stringify(e))
+        this.loader.dismiss()
+      }, () => {
+      });
     } else {
       alert('NFC support not available')
-      this.loader.dismiss()
-      this.tag = {
-        id: 2,
-        nDefMessage: []
-      }
-      this.performOperation()
+      // this.loader.dismiss()
+      // this.tag = {
+      //   id: 2,
+      //   nDefMessage: []
+      // }
+      // this.performOperation()
     }
   }
+}
