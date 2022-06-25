5f5822b2c6dcb5493a2e9239fcb1d7941cdec121
Chris-Laptop <christian.gabler42@gmail.com>
2022-06-26 01:18:41 +0200

    no message

--- server/server.js
+++ server/server.js
@@ -65,18 +65,105 @@ https.createServer(options, app).listen(3000);
 
 
 router.get('/item', (req, res, next) => {
-    console.log('test')
-    db.query(
-      `select * from Item;`,
-      (err, result) => {
-        if (err) {
-          return res.status(400).send({
-            msg: err
-          });
-        } else {
-            console.log(result)
-            res.send(result)
+  let query = 'select * from Item;'
+  basicQuery(query, req, res)
+})
+
+router.get('/itemByUser', (req, res, next) => {
+  let query = 'select * from Item where fidUser = '+mysql.escape(req.query.fidUser)+';'
+  basicQuery(query, req, res)
+})
+
+router.get('/favoritesByUser', (req, res, next) => {
+  let query = 'select * from Item where fidUser = '+mysql.escape(req.query.fidUser)+' and blnFavorite = true;'
+  basicQuery(query, req, res)
+})
+
+router.post('/markAsDirty', (req, res, next) => {
+  let query = 'update Item set blnDirty = true where idItem = '+mysql.escape(req.body.idItem)+';'
+  basicQuery(query, req, res)
+})
+
+router.post('/unmarkAsClean', (req, res, next) => {
+  let query = 'update Item set blnDirty = false where idItem = '+mysql.escape(req.body.idItem)+';'
+  basicQuery(query, req, res)
+})
+
+router.post('/markAsFavorite', (req, res, next) => {
+  let query = 'update Item set blnFavorite = true where idItem = '+mysql.escape(req.body.idItem)+';'
+  basicQuery(query, req, res)
+})
+
+router.post('/unmarkAsFavorite', (req, res, next) => {
+  let query = 'update Item set blnFavorite = false where idItem = '+mysql.escape(req.body.idItem)+';'
+  basicQuery(query, req, res)
+})
+
+router.post('/insertItem', (req, res, next) => {
+  let query = 'insert into Item(idItem, fidUser, tsCreated, tsChanged, tsLastRead, txtName, txtDescription, txtSize, enumCut, setColor, setMaterial, setType, enumCondition, blnDirty, blnFavorite) values ('+
+              uuid()+', '+
+              mysql.escape(req.body.fidUser)+', '+
+              mysql.escape(req.body.tsCreated)+', '+
+              mysql.escape(req.body.tsChanged)+', '+
+              mysql.escape(req.body.tsLastRead)+', '+
+              mysql.escape(req.body.txtName)+', '+
+              mysql.escape(req.body.txtDescription)+', '+
+              mysql.escape(req.body.txtSize)+', '+
+              mysql.escape(req.body.enumCut)+', '+
+              mysql.escape(req.body.setColor)+', '+
+              mysql.escape(req.body.setMaterial)+', '+
+              mysql.escape(req.body.setType)+', '+
+              mysql.escape(req.body.enumCondition)+', '+
+              mysql.escape(req.body.blnDirty)+', '+
+              mysql.escape(req.body.blnFavorite)+' '+
+              ');'
+
+    db.query(query, (err, result) => {
+      if (err) {
+        return res.status(400).send({
+          msg: err
+        });
+      } else {
+        let id = 0;
+        for (var prop in results[0]) {
+            id = results[0][prop]
+            break;
         }
-    })
+
+        let queryImage = 'insert into ItemImage(fidItem, blobImage) values ('+mysql.escape(id)+', '+mysql.escape(req.body.blobImage)+');'
+        db.query(query, (err, result) => {
+          if(err) {
+            return res.status(400).send({
+              msg:err
+            })
+          }
+        })
+
+        let queryWashingInfo = 'insert into ItemWashingInfo(fidItem, nmbTemperature, nmbSpinningCycles) values ('+
+          mysql.escape(id)+', '+
+          mysql.escape(req.body.nmbTemperature)+', '+
+          mysql.escape(req.body.nmbSpinningCycles)+' '+
+          ');'
+
+          db.query(query, (err, result) => {
+            if(err) {
+              return res.status(400).send({
+                msg:err
+              })
+            }
+          })
+      }
   })
-  
+})
+
+basicQuery(query, req, res) {
+  db.query(query, (err, result) => {
+      if (err) {
+        return res.status(400).send({
+          msg: err
+        });
+      } else {
+          res.send(result)
+      }
+  })
+}
