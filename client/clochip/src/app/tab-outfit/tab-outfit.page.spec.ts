f40928e1b91bfc279dd41d3e6dea11efbe826057
akotsovo <73826009+akotsovo@users.noreply.github.com>
2022-07-04 22:23:06 +0200

    renamed favorites to outfit

---
+++ client/clochip/src/app/tab-outfit/tab-outfit.page.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+
+import { TabOutfitPage } from './tab-outfit.page';
+
+describe('TabFavoritesPage', () => {
+  let component: TabOutfitPage;
+  let fixture: ComponentFixture<TabOutfitPage>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [TabOutfitPage],
+      imports: [IonicModule.forRoot(), ExploreContainerComponentModule]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TabOutfitPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
